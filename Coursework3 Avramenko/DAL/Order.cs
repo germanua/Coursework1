fda56175ae7019b9c29b0123e790804c70b6d21e
germanua <114772595+germanua@users.noreply.github.com>
2023-12-21 23:42:57 +0200

    Add files via upload

---
+++ Coursework3 Avramenko/DAL/Order.cs
@@ -0,0 +1,23 @@
+namespace DAL
+{
+    public class Order
+    {
+        public int Id { get; set; } 
+        public List<Dish> Dishes { get; set; } = new ();
+        public int TableNumber { get; set; }
+        public string PaymentMethod { get; set; }
+        public int EstimatedWaitingTime { get; set; }
+        public decimal Total { get; set; }
+
+        public Order() { }
+        public Order(int id, int tableNumber, string paymentMethod, int estimatedWaitingTime, List<Dish> dishes, decimal total)
+        {
+            Id = id;
+            TableNumber = tableNumber;
+            PaymentMethod = paymentMethod;
+            EstimatedWaitingTime = estimatedWaitingTime;
+            Dishes = dishes;
+            Total = total;
+        }
+    }
+}
